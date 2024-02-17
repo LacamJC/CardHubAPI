de57a29c19be951fc3b5f4b66614b54ad2290bc6
LacamJC <joaoramajo744@gmail.com>
2024-02-16 23:51:05 -0300

    Add railway

--- database/conexao.js
+++ database/conexao.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("CardHubApi", "ramajo", "123456", {
+const sequelize = new Sequelize("railway", "root", "b53-66cA2bc214cffAgDcgh3dBAe-2f-", {
     dialect: "mysql",
-    host: "localhost",
-    port: 3306
+    host: "viaduct.proxy.rlwy.net",
+    port: 49332
 })
 
 module.exports = sequelize;
